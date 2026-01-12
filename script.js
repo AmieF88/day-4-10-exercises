bce8f05d5c67925acf1d873b608cbb618a4a529c
AmieF88 <amiemfreeman88@gmail.com>
2026-01-12 17:14:56 -0600

    2nd commit

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ const uppercased = words.map(word => word.toUpperCase());
 console.log(uppercased);
 
 
+
 const nums3 = [1, 2, 3, 4, 5, 6];
 const evens = nums3.filter(num => num % 2 === 0);
 console.log(evens);
